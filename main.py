929b318dada7c96e75e3f970b71aa55004eb89d8
ravigordon <rgordz460@outlook.com>
2022-07-13 15:45:01 +0100

    nested if statement added to check the riders age to determine the cost of the ride

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+age = int(input("What is your age?"))
 if height >= 120:
-  print("You can ride the rollercoster")
+  if age >= 18:
+        print("The price of the rollercoaster will be £12")
+  else:
+        print("The price of the rollercoaster will be £7")
 else:
   print("You are too short , you  cannot ride the rollercoster")
   
