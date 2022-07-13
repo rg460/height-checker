5ee15d0290dff79196f0677f475984d6cf76f9d8
ravigordon <rgordz460@outlook.com>
2022-07-13 15:17:09 +0100

    completed solution

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+  print("You can ride the rollercoster")
+else:
+  print("You are too short , you  cannot ride the rollercoster")
+  
