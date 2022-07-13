4867010573144c744518705954cfae7dbec0a7ba
ravigordon <rgordz460@outlook.com>
2022-07-13 15:20:37 +0100

    logic of if statement changed to include if the users height is 120

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
-if height > 120:
+if height >= 120:
   print("You can ride the rollercoster")
 else:
   print("You are too short , you  cannot ride the rollercoster")
